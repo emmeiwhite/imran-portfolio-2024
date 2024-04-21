a74f9d8cce883593c011d2809c8847f3ec9fe326
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-04-21 13:18:40 +0530

    Navbar Completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,24 @@ function Navbar() {
   return (
     <nav className="bg-emerald-100">
       <div className="mx-auto max-w-7xl px-8 py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8">
-        <span>Imran Logo</span>
-        <p>Links Goes here!</p>
+        <h2 className="text-3xl font-bold">
+          Imran <span className="text-emerald-700">Portfolio</span>
+        </h2>
+
+        <div className="flex gap-x-3 mt-5 sm:mt-0">
+          {links.map((link) => {
+            const { id, href, text } = link;
+            return (
+              <a
+                href={href}
+                key={id}
+                className="capitalize text-lg tracking-wide hover:text-emerald-600 duration-300"
+              >
+                {text}
+              </a>
+            );
+          })}
+        </div>
       </div>
     </nav>
   );

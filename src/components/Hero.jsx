087fbbc4b4d04a71040cc18d97a8158dcb7e93ea
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-04-21 15:09:36 +0530

    Hero Section -- in progress...

---
+++ src/components/Hero.jsx
@@ -0,0 +1,8 @@
+import heroImg from "../assets/hero-2.svg";
+import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from "react-icons/fa";
+
+function Hero() {
+  return <div className="bg-emerald-100 py-24"></div>;
+}
+
+export default Hero;

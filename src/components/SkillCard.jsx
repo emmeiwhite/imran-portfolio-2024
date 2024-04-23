e3579f70dfdb02350f5c7babfafe8e7eae9319bd
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-04-23 20:26:03 +0530

    Text-color in SkillCard.jsx

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -2,7 +2,7 @@ export default function SkillCard({ icon, title, text }) {
   return (
     <article>
       <span>{icon}</span>
-      <h4 className="mt-6 font-bold">{title}</h4>
+      <h4 className="mt-6 font-bold text-emerald-800">{title}</h4>
       <p className="mt-2 text-slate-700 leading-loose">{text}</p>
     </article>
   );

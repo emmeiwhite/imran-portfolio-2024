2847235d2eba3de1a0e7aca2086687f5dd0dd313
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-04-25 22:37:49 +0530

    SectionTitle width fixed

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,6 +1,6 @@
 export default function SectionTitle({ text }) {
   return (
-    <div className="border-b border-gray-200 pb-5 border-b border-emerald-400 ">
+    <div className="border-b-4 pb-3 border-b border-emerald-700 inline-block w-[260px]">
       <h2 className="text-3xl font-medium tracking-wider capitalize text-emerald-900">
         {text}
       </h2>

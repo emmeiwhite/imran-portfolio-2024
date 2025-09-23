8518c171ec9a9786630c2b6d59f577a57ef4a299
emmeiwhite <emmeiwhite@gmail.com>
2025-09-23 23:49:04 +0530

    Title update!

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,7 +1,7 @@
 export default function SectionTitle({ text }) {
   return (
     <div className="border-b-4 pb-3  border-emerald-400 inline-block w-[260px]">
-      <h2 className="text-3xl font-medium tracking-wider capitalize text-emerald-400">{text}</h2>
+      <h2 className="text-3xl font-semibold tracking-wider capitalize text-emerald-400">{text}</h2>
     </div>
   )
 }
